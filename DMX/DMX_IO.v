ed9954940aea1cc147a15c056160142c7e66bc69
EricYangCM <106502039+EricYangCM@users.noreply.github.com>
2025-02-25 18:09:26 +0900

    Update DMX_IO.v

--- DMX/DMX_IO.v
+++ DMX/DMX_IO.v
@@ -2,21 +2,21 @@ module DMX_IO #(
     parameter CLK_FREQ = 12000000,  // 시스템 클럭
     parameter BAUD_RATE = 250000    // DMX 통신 속도
 )(
-    input wire clk,
+    output wire [7:0] LED_TP,	// test LED
+	output wire TP,
+	
+	input wire clk,
     input wire rst_n,
-	input wire dmx_IsTxMode,	// Tx Mode ?
+    input wire dmx_IsTxMode,  // Tx Mode 제어
+    input wire dmx_in,        // DMX 입력 (Rx)
 	
-    output wire dmx_out,   	// DMX 출력 (Tx)
-    output reg [8*512-1:0] dmx_register_Tx, // 1차원 벡터로 선언 (512바이트 데이터 저장)
-	
-	output wire dmx_in,   	// DMX 입력 (Rx)
-	output reg [8*512-1:0] dmx_register_Rx // 1차원 벡터로 선언 (512바이트 데이터 저장)
+	input wire [9:0] dmx_Tx_num_bytes,	// 보낼 데이터 수
+	input wire [1:0] dmx_Tx_Hz,		// Tx 주파수
+
+    output wire dmx_out   	// DMX 출력 (Tx)
 );
 
-    // **DMX Tx 모드 및 제어 변수**
-    reg [1:0] tempModeSelect = 2'b11;  // 기본 40Hz 설정
-    reg [9:0] num_bytes = 512;
-    reg tempEN = 1;
+
 
     // **DMX Tx 인스턴스 (송신 모듈)**
     DMX_Tx #(
@@ -25,19 +25,52 @@ module DMX_IO #(
     ) dmx_tx_inst (
         .clk(clk),
         .rst_n(rst_n),
-        .enable(tempEN),
-        .num_bytes(num_bytes),
-        .dmx_data(dmx_register_Tx), // 1차원 벡터로 전달
-        .mode_select(tempModeSelect),
+        .enable(dmx_IsTxMode),
+        .num_bytes(dmx_Tx_num_bytes),
+		.EBR_Addr(EBR_Addr_A),
+		.EBR_Data(EBR_Data_A),
+        .mode_select(dmx_Tx_Hz),
         .tx(dmx_out),  // Tx 출력 연결
+		.TP(TP),
         .busy()
     );
 
-    // **초기값 설정 (dmx_register_Tx 초기화)**
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            dmx_register_Tx <= {512{8'h00}};  // 전체를 0x00으로 초기화
-        end
-    end
+/*
+
+    // **DMX Rx 인스턴스 (수신 모듈)**
+    DMX_Rx #(
+        .CLK_FREQ(CLK_FREQ),
+        .BAUD_RATE(BAUD_RATE)
+    ) dmx_rx_inst (
+        .LED_TP(LED_TP),		// Test LED
+		.TP(TP),
+		
+		.clk(clk),
+        .rst_n(rst_n),
+        .enable(!dmx_IsTxMode),
+        .dmx_data(rx_data), // RX 데이터 신호 연결
+        .dmx_in(dmx_in)  // Rx 입력 연결
+    );
+	*/
+	
+	// **DMX Tx Dimming Register Map
+	//(* ram_style="block" *) reg [8*513-1:0] Tx_DimmingRegMap;
+	
+	wire[9:0] EBR_Addr_A;
+	wire[7:0] EBR_Data_A;
+	
+	// **EBR 인스턴스 (DMX TX 데이터 저장)**
+    EBR_DMX_Tx_DimmingData tx_dimmingData (
+        // A포트는 DMX TX 모듈이 사용
+        .ClockA(clk),
+        .ClockEnA(1'b1),
+        .WrA(!dmx_IsTxMode),  // DMX TX 모듈이 읽기 또는 쓰기
+        .AddressA(EBR_Addr_A),
+        .QA(EBR_Data_A)
+    );
+	
+	
+	
+	
 
 endmodule
