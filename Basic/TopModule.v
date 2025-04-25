e2946f7bd1e3af9e7506fab0e6db310eb8291678
EricYangCM <106502039+EricYangCM@users.noreply.github.com>
2025-04-25 14:38:04 +0900

    Update TopModule.v

--- Basic/TopModule.v
+++ Basic/TopModule.v
@@ -3,12 +3,13 @@ module TopModule(
     output reg TP
 );
 
-
+// EVB clk-in is pin #27
+// LED : D1 ~ D8 -> 97, 98, 99, 100, 104, 105, 106, 107
+	
 localparam CLK_Freq = 20000000;
 
 
 // PLL for 20MHz
-// EVB clk-in is pin #27
 wire PLL_LOCK;
 wire clk;
 PLL PLL_inst(
